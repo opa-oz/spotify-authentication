ff53824fc55efde160eafdccda71d79b87c4a4be
gahara <madinikus@gmail.com>
2023-02-22 19:05:55 +0900

    implemented callback and token retrival

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,10 @@
 import random
 import os
-from flask import request, redirect, make_response
+import base64
+from flask import request, redirect, make_response, Response
 from urllib.parse import urlencode
+import requests
+
 from app import app
 
 client_id = os.environ.get('client_id')
@@ -19,10 +22,15 @@ def index():
     return 'server running'
 
 
-@app.route('/test')
 def generate_random_string(length=state_length):
     possible_symbols = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
     return ''.join(random.choice(possible_symbols) for i in range(length))
+
+
+@app.route('/tokens')
+def show_tokens():
+    args = request.args
+    return {'access_token': args.get('access_token'), 'refresh_token': args.get('refresh_token')}
 
 
 @app.route('/login')
@@ -32,7 +40,7 @@ def login():
                               'client_id': client_id,
                               'scope': scope,
                               'redirect_uri': redirect_uri,
-                              'state': 'state'
+                              'state': state
                               })
 
     response = make_response(redirect(f'https://accounts.spotify.com/authorize?{query_params}'))
@@ -42,4 +50,36 @@ def login():
 
 @app.route('/callback')
 def callback():
-    return 'callback success'
+    query_params = request.args
+    code = query_params.get('code')
+    state = query_params.get('state')
+    stored_state = request.cookies.get(f'{state_key}')
+    print({'state': state, 'stored_state': stored_state})
+    if not state or state != stored_state:
+        return Response('state mismatch', status=400, )
+    # resp = make_response('http://localhost:8888/callback')
+    # resp.delete_cookie('lol')
+
+    auth_client = f'{client_id}:{client_secret}'
+    auth_encode = 'Basic ' + base64.b64encode(auth_client.encode()).decode()
+    auth_headers = {
+        'Authorization': auth_encode,
+    }
+    data = {
+        'grant_type': 'authorization_code',
+        'redirect_uri': redirect_uri,
+        'code': code
+    }
+
+    token_response = requests.post('https://accounts.spotify.com/api/token', data=data, headers=auth_headers)
+    if token_response.status_code != 200:
+        return {'code': token_response.status_code, 'response': token_response.json()}
+    access_token = token_response.json().get('access_token')
+    refresh_token = token_response.json().get('refresh_token')
+
+    # headers = {'Authorization': f'Bearer {access_token}'}
+    # access_api = requests.get('https://api.spotify.com/v1/me', headers=headers)
+    # if access_api.status_code != 200:
+    #     return {'error': access_api.status_code, 'message': access_api.json()}
+    res = make_response(redirect(f'tokens?access_token={access_token}&refresh_token={refresh_token}'))
+    return res
